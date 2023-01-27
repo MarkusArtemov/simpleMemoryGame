7b49c5751d5f0383f5b37129e6d5c4710b03b289
MarkusArtemov <martemov16@gmail.com>
2023-01-27 12:59:28 +0100

    Nun können nur 2 Karten gleichzeitig aufgedeckt werden

--- puzzle.js
+++ puzzle.js
@@ -6,13 +6,31 @@ let pictureList = ['./pictures/apple.jpeg', './pictures/banana.jpeg','./pictures
 
 const shuffledImages = _.shuffle(pictureList);
 const cards = document.getElementsByClassName('card');
+let numberOfOpenCards = 0;
+
 
 function startGame(){
 for (let i = 0; i < cards.length; i++) {
     cards[i].style.backgroundImage = "url(" + shuffledImages[i] + ")";
-}
+    cards[i].classList.add("card-overlay");
+   setupCards(i);
 }
 
+
+function setupCards(cardIndex){
+      cards[cardIndex].addEventListener("click", function() {
+        if(numberOfOpenCards == 2){
+           numberOfOpenCards = 0;
+            for(let i = 0; i < cards.length; i++){
+                cards[i].style.filter = "brightness(0%)"
+            }
+        }
+        this.style.filter = "brightness(100%)";
+         numberOfOpenCards++;
+    });
+}
+
+}
 startGame();
 
 
